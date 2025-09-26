c97f3adb674cda7002b7caf73996e9e323adad02
rickytheoldtree <rickyliyiqi@outlook.com>
2025-09-26 10:05:57 +0800

    v1.2.0

---
+++ Runtime/Component/RLabel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RicKit.RDebug.Component
+{
+    public class RLabel : MonoBehaviour, IRComponent
+    {
+        private Text textComponent;
+        private Action<RLabel> onUpdate;
+        public void OnUpdate()
+        {
+            onUpdate?.Invoke(this);
+        }
+
+        public string Text
+        {
+            get => textComponent.text;
+            set => textComponent.text = value;
+        }
+        public void Init(string name, int width, int height, int fontSize, Color textColor, Color bgColor, Sprite bgSprite,
+            Action<RLabel> onUpdate)
+        {
+            this.onUpdate = onUpdate;
+            var img = gameObject.AddComponent<Image>();
+            if (bgSprite)
+            {
+                img.sprite = bgSprite;
+                if (bgSprite.border != Vector4.zero)
+                    img.type = Image.Type.Sliced;
+            }
+
+            img.color = bgColor;
+            var rtLabel = gameObject.GetComponent<RectTransform>();
+            rtLabel.sizeDelta = new Vector2(width, height);
+            var goText = new GameObject("Text", typeof(Text));
+            goText.transform.SetParent(transform, false);
+            var rtText = goText.GetComponent<RectTransform>();
+            rtText.anchorMin = Vector2.zero;
+            rtText.anchorMax = Vector2.one;
+            rtText.sizeDelta = Vector2.zero;
+            textComponent = goText.GetComponent<Text>();
+            textComponent.text = name;
+            textComponent.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
+            textComponent.alignment = TextAnchor.MiddleLeft;
+            textComponent.color = textColor;
+            textComponent.fontSize = fontSize;
+        }
+    }
+}
